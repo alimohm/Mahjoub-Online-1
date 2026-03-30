f88db973b61740c2bb389b4ca4e62ab775cfd2ec
alimohm <69640611+alimohm@users.noreply.github.com>
2026-03-30 15:30:39 +0300

    Update wsgi.py

--- core/wsgi.py
+++ core/wsgi.py
@@ -3,13 +3,10 @@ import sys
 from pathlib import Path
 from django.core.wsgi import get_wsgi_application
 
-# الحصول على المسار الرئيسي للمشروع (المكان الذي يوجد فيه manage.py)
+# تأمين المسارات لإنهاء خطأ ModuleNotFoundError
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-# إضافة المسار الرئيسي إلى بيئة بايثون لضمان رؤية مجلد core
 sys.path.append(str(BASE_DIR))
 
-# التأكد من توجيه Django لملف الإعدادات
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_wsgi_application()
