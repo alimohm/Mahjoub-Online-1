a037fd26946edd2bf20035ba429c065b8c86bf68
alimohm <69640611+alimohm@users.noreply.github.com>
2026-03-30 16:11:05 +0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.path),
-    # هنا ستضيف روابط الموردين والمنتجات لاحقاً
+    # تغيير مسار الإدارة التقليدي إلى Dashboard
+    path('dashboard/', admin.site.name == 'Mahjoub Online Dashboard'), 
+    path('admin/', admin.site.urls), # يمكنك تركها أو حذفها
+    
+    # مسارات نظام الموردين والمحفظة الرقمية
+    path('suppliers/', include('suppliers.urls')), 
 ]
