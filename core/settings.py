419ff7b3920b6c989bf487e7fb71a347e1706d08
alimohm <69640611+alimohm@users.noreply.github.com>
2026-03-30 15:26:30 +0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -1,8 +1,14 @@
-import dj_database_url
 import os
+import dj_database_url
+from pathlib import Path
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# إعدادات قاعدة البيانات
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL')
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
     )
 }
