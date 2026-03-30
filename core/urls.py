2bb4d9c9a59cfc38918efd8406b433ac939bc000
alimohm <69640611+alimohm@users.noreply.github.com>
2026-03-30 18:13:27 +0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,8 @@
-from django.contrib import admin
 from django.urls import path, include
 from suppliers.views import dashboard
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('dashboard/', dashboard, name='dashboard'),
-    path('', dashboard), # جعل الداشبورد الصفحة الرئيسية بعد الدخول
+    path('', dashboard), # الصفحة الرئيسية
 ]
