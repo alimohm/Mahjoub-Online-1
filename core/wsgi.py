54728476b08e106d4c8da7027da560199357abb6
alimohm <69640611+alimohm@users.noreply.github.com>
2026-03-30 14:06:27 +0300

    Update wsgi.py

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,14 +1,9 @@
-import os
-import sys
-from pathlib import Path
+import os, sys
 from django.core.wsgi import get_wsgi_application
+from pathlib import Path
 
-# الحصول على مسار المجلد الرئيسي (الذي يحتوي على manage.py)
 BASE_DIR = Path(__file__).resolve().parent.parent
-# إضافة المسار إلى نظام بايثون للبحث
-if str(BASE_DIR) not in sys.path:
-    sys.path.append(str(BASE_DIR))
+sys.path.insert(0, str(BASE_DIR))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-
 application = get_wsgi_application()
