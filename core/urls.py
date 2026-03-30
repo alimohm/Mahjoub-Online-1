2f455306dde412ca9c1bcee65115ef754a1b6ffa
alimohm <69640611+alimohm@users.noreply.github.com>
2026-03-30 17:43:37 +0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
-from django.contrib.auth import views as auth_views
+from suppliers.views import dashboard
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('dashboard/', TemplateView.as_view(template_name='dashboard.html'), name='dashboard'),
-    path('', auth_views.LoginView.as_view(template_name='registration/login.html')), # الصفحة الأولى هي الدخول
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('dashboard/', dashboard, name='dashboard'),
+    path('', dashboard), # جعل الداشبورد الصفحة الرئيسية بعد الدخول
 ]
