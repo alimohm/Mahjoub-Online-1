2f53d6fc9e83b69e534a9795c0d5986186e4e634
alimohm <69640611+alimohm@users.noreply.github.com>
2026-03-30 14:07:28 +0300

    Update models.py

--- suppliers/models.py
+++ suppliers/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Supplier(models.Model):
+class SupplierWallet(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     store_name = models.CharField(max_length=100)
-    wallet = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    balance = models.DecimalField(max_digits=12, decimal_places=2, default=0.00)
     verified = models.BooleanField(default=False)
 
     def __str__(self): return self.store_name
