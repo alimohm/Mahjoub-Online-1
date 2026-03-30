82b7553fd19956c654b7081a8e02ea594c99b2d6
alimohm <69640611+alimohm@users.noreply.github.com>
2026-03-30 12:56:33 +0300

    Update wsgi.py

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,1 +1,9 @@
 
+import os
+from django.core.wsgi import get_wsgi_application
+
+# تأكد أن 'core.settings' تطابق اسم مجلد إعداداتك
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+# Gunicorn يبحث عن هذا المتغير 'application' تحديداً
+application = get_wsgi_application()
