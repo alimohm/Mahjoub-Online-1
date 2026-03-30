2059345bd65930d6ea575102d92051bb89824e09
alimohm <69640611+alimohm@users.noreply.github.com>
2026-03-30 14:08:29 +0300

    Update urls.py

--- suppliers/urls.py
+++ suppliers/urls.py
@@ -1,9 +1,4 @@
 from django.urls import path
-from . import views
+from .views import home_view
 
-app_name = 'suppliers'
-
-urlpatterns = [
-    path('login/', views.supplier_login, name='login'),
-    path('dashboard/', views.supplier_dashboard, name='dashboard'),
-]
+urlpatterns = [path('', home_view, name='home')]
