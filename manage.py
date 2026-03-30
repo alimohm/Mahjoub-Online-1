55a64917aafae9a50ca8c1877dd89ecdadbd76a9
alimohm <69640611+alimohm@users.noreply.github.com>
2026-03-30 13:03:29 +0300

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,2 +1,13 @@
+import os
+import sys
 
-python-3.10.12
+def main():
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError("Django not found.") from exc
+    execute_from_command_line(sys.argv)
+
+if __name__ == '__main__':
+    main()
