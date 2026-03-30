1baac0bccdd1fffda02f5474882bb233c13ca925
alimohm <69640611+alimohm@users.noreply.github.com>
2026-03-30 13:31:15 +0300

    Update wsgi.py

--- core/wsgi.py
+++ core/wsgi.py
@@ -3,9 +3,14 @@ import sys
 from pathlib import Path
 from django.core.wsgi import get_wsgi_application
 
-# إجبار النظام على رؤية المجلد الرئيسي
+# إضافة المسار الرئيسي لذاكرة بايثون
 BASE_DIR = Path(__file__).resolve().parent.parent
 sys.path.insert(0, str(BASE_DIR))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-application = get_wsgi_application()
+
+try:
+    application = get_wsgi_application()
+except Exception as e:
+    print(f"WSGI Critical Error: {e}")
+    raise
