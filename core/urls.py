3549d7640f5a8f244f3ee49386bc3fdc8f5e2e3b
alimohm <69640611+alimohm@users.noreply.github.com>
2026-03-30 12:11:50 +0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,1 +1,9 @@
 
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # ربط روابط الموردين
+    path('suppliers/', include('suppliers.urls')),
+]
