c21be3192e2e406a93da82c6ec904b6a1b528eec
alimohm <69640611+alimohm@users.noreply.github.com>
2026-04-01 13:11:12 +0300

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,14 +1,10 @@
 from database import Vendor
 
 def do_auth(u, p):
-    """التحقق السيادي من الهوية"""
-    # البحث عن المستخدم في المنصة اللامركزية
+    # البحث المباشر في المنصة اللامركزية
     user = Vendor.query.filter_by(username=u).first()
-    
     if not user:
         return {"status": False, "msg": "اسم المستخدم غير مسجل في المنصة اللامركزية"}
-    
     if user.password != p:
         return {"status": False, "msg": "كلمة المرور غير صحيحة"}
-    
     return {"status": True, "user": user}
