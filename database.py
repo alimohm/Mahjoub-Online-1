9cefa9dc9fd96a90ec61da50540aa3ae8328b3c7
alimohm <69640611+alimohm@users.noreply.github.com>
2026-04-04 18:06:59 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,11 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 
-# 1. إنشاء كائن قاعدة البيانات ككائن عالمي (Global Object)
 db = SQLAlchemy()
 
 def init_db(app):
-    """
-    2. ربط قاعدة البيانات بتطبيق Flask الرئيسي عند الإقلاع.
-    هذه الدالة تُستدعى مرة واحدة فقط في ملف app.py.
-    """
     db.init_app(app)
