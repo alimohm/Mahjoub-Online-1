50f0436e81e932e32d85207872048e91ddb9967c
alimohm <69640611+alimohm@users.noreply.github.com>
2026-04-04 17:00:40 +0300

    Update vendor_logic.py

--- vendor_logic.py
+++ vendor_logic.py
@@ -2,22 +2,24 @@ from flask import session
 from models import Vendor, VendorStaff
 
 def login_vendor(u, p):
-    # البحث عن مالك
+    # 1. فحص الملاك
     vendor = Vendor.query.filter_by(username=u).first()
     if vendor and vendor.password == p:
         session.clear()
         session['user_id'] = vendor.id
-        session['role'] = 'vendor_owner' # يرى المحفظة
+        session['role'] = 'vendor_owner'
         session['username'] = vendor.username
-        return True, "مرحباً بك يا سيد السوق الذكي"
+        session['brand'] = vendor.brand_name
+        return True, "مرحباً بك يا سيد السوق الذكي."
 
-    # البحث عن موظف
+    # 2. فحص الموظفين
     staff = VendorStaff.query.filter_by(username=u).first()
     if staff and staff.password == p:
         session.clear()
         session['user_id'] = staff.vendor_id
-        session['role'] = 'vendor_staff' # لا يرى المحفظة
+        session['role'] = 'vendor_staff'
         session['username'] = staff.username
-        return True, "تم دخول الموظف بنجاح"
+        session['brand'] = staff.owner.brand_name
+        return True, "تم دخول الموظف بنجاح."
         
-    return False, "بيانات الدخول غير صحيحة"
+    return False, "اسم المستخدم أو كلمة المرور غير صحيحة."
